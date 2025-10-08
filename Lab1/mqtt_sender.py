43ee936407a22bc3cc0bc7ac59a3d1dedf78a784
samuvazfez <samuel.vazquez.fernandez@udc.es>
2025-10-08 15:59:26 +0200

    feat: add interactive TOR message sender

---
+++ Lab1/mqtt_sender.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import tor
+import time
+from config import BROKER_HOST, BROKER_USER, BROKER_PWD, BROKER_PORT, KEEPALIVE, MY_ID
+
+# Configuracion del cliente 
+def mqtt_client (server, port, topic, user, password, keepalive):
+    client = mqtt.Client()
+    client.username_pw_set(user, password)
+    client.connect(server, port, keepalive)
+    client.subscribe(topic)
+    return client
+
+# Creamos el ciente MQTT
+if __name__ == "__main__":
+    client = mqtt_client(BROKER_HOST, BROKER_PORT, MY_ID, BROKER_USER, BROKER_PWD, KEEPALIVE)
+
+    # Ejemplo de mensaje y ruta (ajusta según necesites)
+    cipher_message = b"PrimeraPrueba de mensaje super secreto"
+    path = ["svf", "ancr", "svf", "ancr"]  
+
+    encrypted_to_send = tor.encrypt_nested_hybrid(path, cipher_message)
+
+    # Iniciar loop para que el hilo de red de paho procese la publicación
+    client.loop_start()
+    
+    print("SENDER: len:", len(encrypted_to_send))
+    print("SENDER: prefix hex:", encrypted_to_send[:64].hex())
+
+    publish_result = client.publish(path[0], payload=encrypted_to_send, qos=1)
+    
+    publish_result.wait_for_publish()
+    time.sleep(0.1)  
+    client.loop_stop()
+    client.disconnect()
+
+    print(f"Mensaje cifrado publicado en el topic '{path[0]}'")
